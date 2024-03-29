a62bbd15d4705717cbfbe041c71d55ef46d8fb27
Rohit Kapkoti <rohitkapkoti0@gmail.com>
2024-03-30 05:21:59 +0530

    fix:baseurl

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://task-management-backend-mocha.vercel.app/api';
 
 const apiCalls = {
     // Fetch all tasks
